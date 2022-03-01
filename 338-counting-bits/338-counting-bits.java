aecc3df988bd005546f1ac01efb8199920c45c27
ghost_32 <kalash.strt@gmail.com>
2022-03-01 14:12:18 +0530

    Time: 2 ms (82.30%), Space: 48.8 MB (7.76%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,22 +1,30 @@
 class Solution {
-    public int bits(int num){
+    public int bits(int num, int[] memo){
         if(num == 0)
             return 0;
         
         if(num == 1)
             return 1;
         
-        if((num&1) != 1)
-            return bits(num/2);
-            
-        return 1 + bits(num/2);
+        if(memo[num] != 0)
+            return memo[num];
+        
+        if((num&1) != 1){
+            int solve = bits(num/2, memo);
+            memo[num] = solve;
+            return solve;
+        }else{
+            int solve = 1 + bits(num/2, memo);
+            memo[num] = solve;
+            return solve;
+        }
     }
     
     public int[] countBits(int n) {
         int[] result = new int[n+1];
         
         for(int i=0; i<n+1; i++){
-            result[i] = bits(i);
+            result[i] = bits(i, result);
         }
         
         return result;
