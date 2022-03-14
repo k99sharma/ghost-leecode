83b81e34ed7fa8a7f8cb034e5f96711b11f08e8c
ghost_32 <kalash.strt@gmail.com>
2022-03-15 02:05:41 +0530

    Time: 2 ms (83.61%), Space: 57.4 MB (6.91%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,24 +1,32 @@
+// Borye - Moore Majority Voting Algorithm
+
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<> ();
+        int count = 0, candidate = -1;
         
-        // creating hash map
-        for(int x : nums){
-            if(hm.containsKey(x))
-                hm.put(x, hm.get(x) + 1);
-            else
-                hm.put(x, 1);
-        }
-        
-        int cond = nums.length/2;
-        int ele = 0;
-        for(Map.Entry<Integer, Integer> entry : hm.entrySet()){
-            if(entry.getValue() > cond){
-                ele = entry.getKey();
-                break;
+        // finding majority
+        for(int i=0; i<nums.length; i++){
+            if(count == 0){
+                candidate = nums[i];
+                count = 1;
+            }else{
+                if(candidate == nums[i])
+                    count++;
+                else
+                    count--;
             }
         }
         
-        return ele;
+        // getting total count of majority
+        count = 0;
+        for(int i=0; i<nums.length; i++)
+            if(nums[i] == candidate)
+                count++;
+        
+        // checking if count is greater than N/2
+        if(count > nums.length/2)
+            return candidate;
+        
+        return -1;
     }
 }
