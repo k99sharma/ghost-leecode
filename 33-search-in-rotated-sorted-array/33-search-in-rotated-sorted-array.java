cb1c93144eb77bc8af482e899f2a5b83bafb204a
ghost_32 <kalash.strt@gmail.com>
2022-03-19 00:37:49 +0530

    Time: 1 ms (67.95%), Space: 43.5 MB (6.05%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,52 +1,29 @@
 class Solution {
-    public int binarySearch(int[] nums, int target, int start, int end){
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-                end = mid-1;
-            else 
-                start = mid+1;
-        }
-        
-        return -1;
-    }
-    
     public int search(int[] nums, int target) {
+        // if nums is empty
         if(nums.length == 0)
             return -1;
         
-        // find rotated points
-        int maxPoint = -1;
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] > nums[i+1]){
-                maxPoint = i;
-                break;
-            }
+        int start = 0;
+        int end = nums.length-1;
+        int pivot = nums[0];
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[start] == target)
+                return start;
+            
+            else if(nums[end] == target)
+                return end;
+            
+            else if(nums[mid] == target)
+                return mid;
+            
+            else if(target > nums[mid]) end--;
+            else start++;
         }
         
-        if(maxPoint == -1){
-            return binarySearch(nums, target, 0, nums.length-1);
-        }
-                
-        int start =0, end = 0;
-        
-        if(target == nums[maxPoint])
-            return maxPoint;
-        
-        else if(target < nums[0]){
-            start = maxPoint+1;
-            end = nums.length-1;
-        }else{
-            start = 0;
-            end = maxPoint;
-        }
-
-        int result = binarySearch(nums, target, start, end);
-        
-        return result;
-        
+        return -1;
     }
 }
