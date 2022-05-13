ced02d50fddfa5094e319dd829f30bae8803f669
ghost_32 <kalash.strt@gmail.com>
2022-05-13 23:15:15 +0530

    Time: 21 ms (35.58%), Space: 42.2 MB (22.67%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,30 +1,38 @@
 class Solution {
     public void solveSudoku(char[][] board) {
+        // we are trying to solve the board
         if(!solve(board))
             System.out.println("Cannot be solved");
     }
     
     public boolean solve(char[][] board){
+        // traversing each cell in board
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
+                // if cell is empty
                 if(board[i][j] == '.'){
+                    // we are gonna try every character for that cell
                     for(char c='1'; c<='9'; c++){
+                        // if character is valid in that cell
                         if(isValid(board, c, i, j)){
-                            board[i][j] = c;
+                            board[i][j] = c; // put it
                             
+                            // try solving the board with updation of this cell
                             if(solve(board))
                                 return true;
                             
                             else
-                                board[i][j] = '.';
+                                board[i][j] = '.'; // if cannot solve remove the value placed
                         }
                     }
                     
+                    // if cannot solve the whole board
                     return false;
                 }
             }
         }
         
+        // if whole board is solved
         return true;
     }
     
