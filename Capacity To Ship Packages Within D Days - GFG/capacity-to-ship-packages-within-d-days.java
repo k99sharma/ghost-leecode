1af84366a5e852895cada7b537208aae180b704d
Kalash Sharma <kalash.strt@gmail.com>
2022-10-26 00:59:35 +0530

    Added solution - LeetHub

---
+++ Capacity To Ship Packages Within D Days - GFG/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            int[] arr = new int[N];
+            
+            int D = Integer.parseInt(read.readLine());
+            
+            for(int i=0; i<N; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.leastWeightCapacity(arr,N,D));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int leastWeightCapacity(int[] arr, int N, int D) {
+        int left = 0, right = 0;
+        for (int w: arr) {
+            left = Math.max(left, w);
+            right += w;
+        }
+        while (left < right) {
+            int mid = (left + right) / 2, need = 1, cur = 0;
+            for (int w: arr) {
+                if (cur + w > mid) {
+                    need += 1;
+                    cur = 0;
+                }
+                cur += w;
+            }
+            if (need > D) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+};
