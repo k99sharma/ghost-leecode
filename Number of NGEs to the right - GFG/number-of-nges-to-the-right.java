3569be505088b2aa12fc11a099e3d836ca9592dc
Kalash Sharma <kalash.strt@gmail.com>
2022-12-04 20:38:25 +0530

    Added solution - LeetHub

---
+++ Number of NGEs to the right - GFG/number-of-nges-to-the-right.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+      }
+      int q = sc.nextInt();
+      int ind[]=new int[q]; 
+      for(int i=0;i<q;i++) ind[i]=sc.nextInt();
+      Solution obj = new Solution();
+      int ans[] = obj.count_NGEs(n,a,q,ind);
+      for(int e : ans)
+      System.out.print(e+" ");
+      System.out.println();
+    }
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public static int findCountNGE(int[] arr, int idx){
+        int count = 0;
+        int curr = arr[idx];
+        
+        for(int i=idx+1; i<arr.length; i++){
+            if(arr[i] > curr) count++;    
+        }   
+        
+        return count;
+    }
+    
+  public static int[] count_NGEs(int N, int arr[], int queries, int indices[]) {
+    // code here
+    int[] result = new int[queries];
+    
+    for(int q=0; q<queries; q++){
+        result[q] = findCountNGE(arr, indices[q]);
+    }
+      
+     return result;
+  }
+}
+     
